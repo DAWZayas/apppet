141002571f546e8fcb2b23d881b0b46487ef3c54
Jesús <jesusmartorrecilla@gmail.com>
2017-11-28 21:36:34 +0100

    Edited footer

--- components/common/FooterComponent.vue
+++ components/common/FooterComponent.vue
@@ -1,7 +1,15 @@
 <template>
     <footer>
-		<p> Ut fermentum leo sed massa iaculis tincidunt. </p>
-	</footer>  
+		<div class="apppet">AppPet 2017 ©</div>
+    <div class="rss">
+      <a class="twitter" href="#"><img src="~/assets/media/twitter.png" class="rss-logo"></a>
+      <a class="facebook" href="#"><img src="~/assets/media/facebook.png" class="rss-logo"></a>
+      <a class="google" href="#"><img src="~/assets/media/google-plus.png" class="rss-logo"></a>
+      <a class="instagram" href="#"><img src="~/assets/media/instagram.png" class="rss-logo"></a>
+
+    </div>
+    <div class="create">Designed by: Jesús, Evelin & Israel</div>
+	</footer>
 </template>
 <script>
 
@@ -14,9 +22,16 @@ footer {
 	padding: 1em;
 	color: #fff;
 	background-color: #000;
+  display: flex;
+  justify-content: space-around;
+}
+.rss-logo{
+  width: 30px;
+  margin: 0 1em;
+
+
 }
 
-/* @MEDIA */
 @media screen and (max-width: 800px) {
 	footer {
 		display: none;

f9fb9503256a764d7288bcf3e2775e03cfb74ae1
iamh <iamh3891@gmail.com>
2018-01-08 14:14:42 +0100

    Login with network socials

--- components/landing/LogIn.vue
+++ components/landing/LogIn.vue
@@ -16,11 +16,18 @@
       Recordar contraseña
     </label>
   </div>
-  <button @click="login" class="btn btn-default buttons button-submit">Ingresar</button>
+  <div>
+    <button @click="login" class="btn btn-default buttons button-submit">Ingresar</button>
+    <p>o</p>
+    <button class="loginBtn loginBtn--facebook" @click="loginWithFacebook">Login with Facebook</button>
+    <button class="loginBtn loginBtn--google" @click="loginWithGoogle">Login with Google</button>
+  </div>
 </div>
 
 </template>
 <script>
+  import firebase from 'firebase'
+  import firebaseApp from '~/firebaseapp'
   import {mapActions, mapGetters} from 'vuex'
   export default {
     data () {
@@ -37,6 +44,21 @@
       login () {
         let method = this.authenticate
         method({email: this.email, password: this.password})
+      },
+      loginWithFacebook () {
+        this.loginWithProvider(new firebase.auth.FacebookAuthProvider())
+      },
+      loginWithGoogle () {
+        this.loginWithProvider(new firebase.auth.GoogleAuthProvider())
+      },
+      loginWithProvider (provider) {
+        firebaseApp.auth().signInWithPopup(provider)
+        firebaseApp.auth().getRedirectResult()
+          .then(() => this.$router.push('/'))
+          .catch((error) => { this.error = error })
+      },
+      setError (error) {
+        this.error = error.message
       }
     }
   }
@@ -47,6 +69,11 @@
 		font-family: "pokemonsolid";
 		src: url(~/assets/font/pokemonsolid.ttf) format("truetype");
 	}
+  p {
+    color: white;
+    text-align: center;
+    margin-bottom: 0.5em;
+  }
 	.input-text {
 		transition: .8s;
     background-color: rgba(0,0,0,.5);
@@ -83,5 +110,59 @@
   .input-text:focus {
 		transition: .8s;
 		border-bottom-color: white;
+  }
+  /* login with network socials */
+  .loginBtn {
+    position: relative;
+    width: 100%;
+    height: 30px;
+    padding-left: 2.5em;
+    border: none;
+    text-align: left;
+    line-height: 34px;
+    white-space: nowrap;
+    border-radius: 0.2em;
+    font-size: 16px;
+    color: #FFF;
+  }
+  .loginBtn:hover{
+    cursor:pointer;
+  }
+  .loginBtn:before {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 34px;
+    height: 100%;
+  }
+  .loginBtn:focus {
+    outline: none;
+  }
+  .loginBtn:active {
+    box-shadow: inset 0 0 0 32px rgba(0, 0, 0, 0.1);
+  }
+  /* Facebook */
+  .loginBtn--facebook {
+    background-color: #4C69BA;
+  }
+  .loginBtn--facebook:before {
+    border-right: #364e92 1px solid;
+    background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
+  }
+  .loginBtn--facebook:hover, .loginBtn--facebook:focus {
+    background-color: #5B7BD5;
+  }
+  /* Google */
+  .loginBtn--google {
+    background: #DD4B39;
+  }
+  .loginBtn--google:before {
+    border-right: #BB3F30 1px solid;
+    background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png') 6px 6px no-repeat;
+  }
+  .loginBtn--google:hover, .loginBtn--google:focus {
+    background: #E74B37;
   }
 </style>

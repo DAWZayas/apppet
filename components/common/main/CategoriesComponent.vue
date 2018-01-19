048bbfa54eab58cc7ec89d531483ade9add10e0d
“evelin” <evelinelizeth@gmail.com>
2018-01-19 09:39:22 +0100

    Changes in CategoriesComponent

--- components/common/main/CategoriesComponent.vue
+++ components/common/main/CategoriesComponent.vue
@@ -1,21 +1,36 @@
 <template>
-    <div class="section-one">
-      <h1>Nuestras Mascotas</h1>
-      <div class="categories">
-        <div class="category">
-          <nuxt-link to="/mainpets/adoption"><img src="~/assets/candidatos/modal3.jpg"></nuxt-link>
-          <h2>En adopción</h2>
-        </div>   
-        <div class="category">  
-          <nuxt-link to="/mainpets/lost"><img src="~/assets/candidatos/modal4.jpg"></nuxt-link>
-          <h2>Perdidos</h2>
-        </div>  
-        <div class="category">  
-          <nuxt-link to="/mainpets/takeCare"><img src="~/assets/candidatos/modal2.jpg"></nuxt-link>
-          <h2>Cuidar</h2> 
+  <div class="section">
+    <h1>Nuestras Mascotas</h1>
+    <div class="categories">
+      <div class="card" style="width: 25rem;">
+        <img class="card-img-top" src="~/assets/candidatos/modal3.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">En adopción</h5>
+          <p class="card-text">Que un perro o un gato no acabe en la calle depende de nosotros. 
+            Ellos buscan una segunda oportunidad</p>
+          <nuxt-link to="/mainpets/adoption" class="btn btn-primary">Ver más</nuxt-link>
+        </div>
+      </div>
+      <div class="card" style="width: 25rem;">
+        <img class="card-img-top" src="~/assets/candidatos/modal4.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Perdidos</h5>
+          <p class="card-text">Encontraste o perdiste una mascota? Aqui tienes todos los aniamles perdidos y encontrados. 
+            ¡Consulta ya los anuncios !.</p>
+          <nuxt-link to="/mainpets/lost" class="btn btn-primary">Ver más</nuxt-link>
+        </div>
+      </div>
+      <div class="card" style="width: 25rem;">
+        <img class="card-img-top" src="~/assets/candidatos/modal2.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">A cuidar</h5>
+          <p class="card-text">¿Tienes que irte de viaje? Encuentra a personas en tu misma situación,
+            y llega a un acuerdo para cuidar a tu mascota.</p>
+          <nuxt-link to="/mainpets/takeCare" class="btn btn-primary">Ver más</nuxt-link>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -37,8 +52,22 @@ export default {
   box-sizing: border-box;
 }
 
-.section-one {
+.section {
   text-align: center;
+  padding: 5% 0;
+}
+
+h1 {
+  font-size: 3rem;
+  font-weight: 700;
+}
+
+h5 {
+  font-size: 2rem;
+}
+
+h5, p {
+  font-weight: 600;
 }
 .categories {
   width: 90%;
@@ -48,22 +77,24 @@ export default {
   flex-wrap: wrap;
   justify-content: center; 
 }
+.card {
+  margin: 1%;
+  background-color: #edf8f7;
+  box-shadow: 5px 7px #f2f2f2;
+}
+@media (min-width: 1024px){
+  
+  .section {
+    padding: 2% 0;
+  }
 
-.category {
-  background-color: rgb(7, 78, 119);
-  border: 1px solid grey;
+	.categories {
+    width: 78%;
+    justify-content: space-around;
+  }
+
+  .card {
   margin: 3%;
-  opacity: 1;
-  color: white;
+  }
 }
-
-.category img {
-  width: 100%;
-  max-width: 300px;
-}
-
-.category:hover {
-  opacity: 0.7;
-}
-
 </style>

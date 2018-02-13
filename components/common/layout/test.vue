80f714906f8bebd816d3a2b4b376b1f979e944e4
Jesús Martínez Torrecilla <jesusmartorrecilla@gmail.com>
2018-02-13 13:08:10 +0100

    Adding style to single page animal

--- components/common/layout/test.vue
+++ components/common/layout/test.vue
@@ -1,12 +1,51 @@
 <template>
-  <div>{{ name }}</div>
+<v-layout row class="card">
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-media
+          :src="photo"
+          height="300px"
+        >
+          <v-chip class="avatar-owner">
+            <v-avatar>
+              <img :src="photo" alt="avatar" class="avatar-border">
+            </v-avatar>
+            <span class="black--text">Jesus Mártinez Torrecilla</span>
+          </v-chip>
+        </v-card-media>
+        <v-card-title primary-title class="first">
+          <div class="d-inline-flex buttons">
+            <v-btn small outline color="indigo" class="chat">Chat</v-btn>
+            <v-btn small outline color="indigo" class="chat">Adoptar</v-btn>
+            <v-btn small outline color="indigo" class="chat">Denunciar</v-btn>
+          </div>
+          <div>
+            <div class="headline">Neo - Perro</div>
+            <span class="grey--text">Perdido desde:16-09-2200</span>
+          </div>
+        </v-card-title>
+        <hr>
+        <v-card-actions>
+          <v-btn flat>Compartir</v-btn>
+          <v-btn flat color="purple">Compartir</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+        <hr>
+          <v-card-text>
+            Hola
+          </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
 import {mapActions, mapGetters} from 'vuex'
 export default {
   data () {
     return {
-      name: ''
+      name: '',
+      photo: '',
+      show: false
     }
   },
   computed: {
@@ -19,13 +58,45 @@ export default {
     this.petSinglePage(this.$route.params.id)
   },
   watch: {
-    animal () { this.name = this.animal.nameAnimal }
+    animal () {
+      this.name = this.animal.nameAnimal
+      this.photo = this.animal.animalPhoto[0]
+    }
   }
 }
 </script>
 
 <style scoped>
-div{
-  margin-top: 80px;
+
+.layout{
+  width: 100%!important;
+  margin:0!important;
 }
+
+.card{
+  margin-top: 55px;
+}
+
+.avatar-owner{
+  margin-top: 230px;
+  margin-bottom: 30px;
+  margin-left: 20px;
+}
+.avatar-border{
+  width: 150%!important;
+  height: 150%!important;
+  border: 3px solid white;
+}
+
+hr{
+  margin: 1em 1.5em;
+}
+.first{
+  padding-top: 0;
+}
+
+.buttons{
+  width: 100%;
+}
+
 </style>
